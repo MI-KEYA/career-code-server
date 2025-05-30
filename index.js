2969f045d70f7e31bd06d8d763c7fde79e259cb4
MI-KEYA <moniakeya913@gmail.com>
2025-05-30 10:29:21 +0600

    did the basic express setup connected mongodb created database and get data

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// dbUser
+// IcZ6m0eicwsfRNjD
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5wsrv4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const jobsCollection = client.db('careerCode').collection('jobs');
+    // jobs api
+    app.get('/jobs', async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Career Code server Running!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
